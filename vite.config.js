99f4864a0bb898c8ea65708fddf40af096b3c0d2
Idan <idan.h@squaretalk.com>
2025-07-15 01:21:39 +0300

    Cleanup for redudent files

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base:'https://lukasheyal.github.io/instaCat/',
     plugins: [react()],
     build: {
-        outDir: 'dist',
+        outDir: '../backend/public',
         emptyOutDir: true
     }
 })
-
-
