50c7e4cc7fa824284b5cedb2b75d20ee8221b82f
Idan <idan.h@squaretalk.com>
2025-07-16 00:18:21 +0300

    Progress on the sidebar cmp

--- src/cmps/SideBar.jsx
+++ src/cmps/SideBar.jsx
@@ -1,8 +1,18 @@
-export function SideBar() {
+import { Link } from 'react-router-dom'
 
-    return (
-        <section className="side-bar">
-            Test
-        </section >
-    )
+export function SideBar() {
+  return (
+    <section className="side-bar">
+      <div className="logo">
+        <img src="../../public/img/instagram-logo.png" alt="instagram-logo" />
+      </div>
+      <nav>
+      <Link to="/"><div className="sidebar-home">Home</div></Link>
+      <Link to="/explore"><div className="sidebar-explore">Explore</div></Link>
+      <Link to="/reels"><div className="sidebar-reels">Reels</div></Link>
+      <Link to="/messages"><div className="sidebar-messages">Messages</div></Link>
+      <Link to="/profile/123"><div className="sidebar-profile">Profile</div></Link>
+      </nav>
+    </section>
+  )
 }
