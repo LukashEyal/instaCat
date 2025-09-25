eaefba987e6abc311607c3a9c6c77505f7f18dba
LukashEyal <lukash.eyal@gmail.com>
2025-09-25 09:31:41 +0300

    dss

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,13 +1,19 @@
 // Explore.jsx
-import { useEffect, useMemo, useState } from "react"
-import { postService } from "../services/posts/post.service"
-import { useDispatch, useSelector } from "react-redux"
-import { SET_POSTS } from "../store/posts.reducer"
-import { ExplorePost } from "./ExplorePost"
-import { ExplorePostModal } from "../cmps/post/ExplorePostModal.jsx"
+import { useEffect, useMemo, useState } from 'react'
+import { postService } from '../services/posts/post.service'
+import { useDispatch, useSelector } from 'react-redux'
+import { SET_POSTS } from '../store/posts.reducer'
+import { ExplorePost } from './ExplorePost'
+import { PostModalView } from '../cmps/post/PostModalView'
+import { Comments } from '../cmps/post/PostModal'
+import { loadUsers } from '../store/user.actions'
+import { loadPosts } from '../store/posts.actions'
+// import { ExplorePostModal } from "../cmps/post/ExplorePostModal.jsx"
 
 export function Explore() {
-  const posts = useSelector((store) => store.postsModule.posts)
+  const loggedInUser = useSelector(s => s.userModule.user)
+  const posts = useSelector(s => s.postsModule.posts) || []
+  const users = useSelector(s => s.userModule.users) || []
   const [openPostId, setOpenPostId] = useState(null)
   const dispatch = useDispatch()
 
@@ -17,8 +23,10 @@ export function Explore() {
   }
 
   useEffect(() => {
+    loadPosts()
+    loadUsers()
     if (posts.length) return
-    loadExplorePosts().catch((err) => console.log("Cannot load posts", err))
+    loadExplorePosts().catch(err => console.log('Cannot load posts', err))
   }, [])
 
   const shuffled = useMemo(() => {
@@ -30,24 +38,31 @@ export function Explore() {
     return a
   }, [posts])
 
-  const openPost = openPostId ? posts.find((p) => p._id === openPostId) : null
+  const openPost = openPostId ? posts.find(p => p._id === openPostId) : null
+  const openPostUser = openPost ? users.find(u => u._id === openPost.userId) : null
 
   return (
-    <div className='explore-container'>
-      <div className='explore-mid-container'>
-        {shuffled.map((post) => (
-          <ExplorePost
-            post={post}
-            key={post._id}
-            onOpen={() => setOpenPostId(post._id)}
-          />
-        ))}
+    <div className="explore-container">
+      <div className="explore-mid-container">
+        {shuffled.map(post => {
+          const postUser = users.find(user => user._id === post.userId)
+          return (
+            <ExplorePost
+              key={post._id}
+              post={post}
+              postUser={postUser}
+              onOpen={() => setOpenPostId(post._id)}
+            />
+          )
+        })}
       </div>
 
       {openPost && (
-        <ExplorePostModal
+        <Comments
           post={openPost}
-          onBackDropClick={() => setOpenPostId(null)}
+          user={loggedInUser}
+          postUserObj={openPostUser} // ✅ use correct variable
+          onClose={() => setOpenPostId(null)}
         />
       )}
     </div>
